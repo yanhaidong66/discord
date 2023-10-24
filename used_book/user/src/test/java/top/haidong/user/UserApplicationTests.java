9e6f82dec543f62b7f49ef234122f9808e991806
hd <1178258121@qq.com>
2023-10-24 23:07:05 +0800

    init

---
+++ used_book/user/src/test/java/top/haidong/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package top.haidong.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
