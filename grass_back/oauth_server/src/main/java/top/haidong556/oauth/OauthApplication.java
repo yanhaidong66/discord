c7408132ec95e881c945d4e6efc922607925009a
hd <1178258121@qq.com>
2023-11-28 00:04:49 +0800

    oauth debug

--- grass_back/oauth_server/src/main/java/top/haidong556/oauth/OauthApplication.java
+++ grass_back/oauth_server/src/main/java/top/haidong556/oauth/OauthApplication.java
@@ -1,6 +1,7 @@
 package top.haidong556.oauth;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
