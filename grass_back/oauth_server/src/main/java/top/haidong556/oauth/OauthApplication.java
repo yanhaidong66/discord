53ae214336806c3f3f9ea873ba49b2d0e9d7d57e
hd <1178258121@qq.com>
2023-11-27 19:33:14 +0800

    eureka

---
+++ grass_back/oauth_server/src/main/java/top/haidong556/oauth/OauthApplication.java
@@ -0,0 +1,18 @@
+package top.haidong556.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@EnableWebSecurity
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthApplication.class, args);
+    }
+
+}
